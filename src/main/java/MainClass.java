cb83992e2123d326e8571f47abc70c7cd868e087
oknion <oknion@gmail.com>
2016-06-16 22:42:07 +0700

    add HelloServlet

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,11 +1,17 @@
 import org.apache.log4j.Logger;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.Jetty;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -23,15 +29,28 @@ public class MainClass {
         System.out.println(getenv);
 //        int port = Integer.parseInt(getenv);
         Server server = new Server(8027);
-        ResourceHandler resourceHandler = new ResourceHandler();
-        resourceHandler.setResourceBase(".");
+//        ResourceHandler resourceHandler = new ResourceHandler();
+//        resourceHandler.setResourceBase(".");
 
-        server.setHandler(new HelloHandler());
+
+        ContextHandlerCollection contextHandlerCollection = new ContextHandlerCollection();
+        ServletContextHandler servletContextHandler = new ServletContextHandler();
+        servletContextHandler.addServlet(new ServletHolder(new HelloServlet()), "/");
+        contextHandlerCollection.setHandlers(new Handler[]{servletContextHandler});
+        server.setHandler(contextHandlerCollection);
         server.start();
 
         server.join();
 
 
+    }
+}
+
+class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello Servlet!!");
+        super.doGet(req, resp);
     }
 }
 
